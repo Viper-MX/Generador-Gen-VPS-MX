6afba55fc2613f9751c1c6dfd35c2092d5edb4c2
Viper-MX <70416261+Viper-MX@users.noreply.github.com>
2021-07-14 00:25:35 -0500

    Update gerar.sh

--- gerador/gerar.sh
+++ gerador/gerar.sh
@@ -369,7 +369,7 @@ read -p "Enter"
 uninstal_master () {
 CIDdir=/etc/ADM-db && [[ ! -d ${CIDdir} ]] && mkdir ${CIDdir}
 
-[[ ! -e "${CIDdir}/confbot.sh" ]] && wget -O ${CIDdir}/confbot.sh https://raw.githubusercontent.com/rudi9999/TeleBotGen/master/confbot.sh &> /dev/null && chmod +x ${CIDdir}/confbot.sh
+[[ ! -e "${CIDdir}/confbot.sh" ]] && wget -O ${CIDdir}/confbot.sh https://raw.githubusercontent.com/Viper-MX/Generador-Gen-VPS-MX/master/confbot.sh &> /dev/null && chmod +x ${CIDdir}/confbot.sh
 
 source ${CIDdir}/confbot.sh
 
